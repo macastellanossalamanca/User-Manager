59bb071015a76a664737090938d799e35f2034a0
macastellanossalamanca <macastellanossalamanca@gmail.com>
2025-03-29 10:40:11 -0500

    Folder Structure and Model

---
+++ UsersManager/UsersManager/Utilities/OSLogExtension.swift
@@ -0,0 +1,18 @@
+//
+//  OSLogExtension.swift
+//  UsersManager
+//
+//  Created by Sandra Salamanca on 29/03/25.
+//
+import OSLog
+
+// MARK: - OSLog Extension
+
+extension OSLog {
+    private static var subsystem = Bundle.main.bundleIdentifier!
+
+    static let navigation = OSLog(subsystem: subsystem, category: "Navigation")
+    static let businessLogic = OSLog(subsystem: subsystem, category: "BusinessLogic")
+    static let view = OSLog(subsystem: subsystem, category: "View")
+    static let network = OSLog(subsystem: subsystem, category: "Network")
+}
