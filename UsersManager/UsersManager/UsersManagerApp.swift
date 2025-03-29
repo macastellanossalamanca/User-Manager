41c04f66f7489afe79df2ca02d2a9f066ff54444
macastellanossalamanca <macastellanossalamanca@gmail.com>
2025-03-29 09:56:07 -0500

    Base App
    First commit with base structure

---
+++ UsersManager/UsersManager/UsersManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  UsersManagerApp.swift
+//  UsersManager
+//
+//  Created by Sandra Salamanca on 29/03/25.
+//
+
+import SwiftUI
+
+@main
+struct UsersManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ UsersManager/UsersManagerTests/UsersManagerTests.swift
@@ -0,0 +1,17 @@
+//
+//  UsersManagerTests.swift
+//  UsersManagerTests
+//
+//  Created by Sandra Salamanca on 29/03/25.
+//
+
+import Testing
+@testable import UsersManager
+
+struct UsersManagerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
